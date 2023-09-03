c99edb3f219403955d0595b26aebbf8060f0e7e4
Dan Chui <dchui108@gmail.com>
2023-09-03 22:24:36 +0900

    edit dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's Activity</div>
+      <div>Duration</div>
+      <div>Chart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
