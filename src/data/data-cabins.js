1ef8bdad32cd9cf372ab91f8c32ac596697847da
Dan Chui <dchui108@gmail.com>
2023-10-02 09:10:04 +0900

    edit guest and cabin data

--- src/data/data-cabins.js
+++ src/data/data-cabins.js
@@ -8,71 +8,71 @@ export const cabins = [
     maxCapacity: 2,
     regularPrice: 250,
     discount: 0,
-    image: imageUrl + "cabin-001.jpg",
+    image: imageUrl + "cabin-standard.webp",
     description:
-      "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
+      "Built directly on the white sand beach, the elegant Luxury Beach Front Bungalows offer a unique panoramic view on the island and the protected turquoise lagoon. Each bungalow can accommodate up to a king size bed and additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "002",
     maxCapacity: 2,
     regularPrice: 350,
     discount: 25,
-    image: imageUrl + "cabin-002.jpg",
+    image: imageUrl + "cabin-standard.webp",
     description:
-      "Escape to the serenity of nature and indulge in luxury in our cozy cabin 002. Perfect for couples, this cabin offers a secluded and intimate retreat in the heart of a picturesque forest. Inside, you will find warm and inviting interiors crafted from high-quality wood, a comfortable living area, a fireplace and a fully-equipped kitchen. The luxurious bedroom features a plush king-size bed and spa-like shower. Relax on the private deck with hot tub and take in the beauty of nature.",
+      "Built directly on the white sand beach, the elegant Luxury Beach Front Bungalows offer a unique panoramic view on the island and the protected turquoise lagoon. Each bungalow can accommodate up to a king size bed and additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "003",
     maxCapacity: 4,
     regularPrice: 300,
     discount: 0,
-    image: imageUrl + "cabin-003.jpg",
+    image: imageUrl + "cabin-standard.webp",
     description:
-      "Experience luxury family living in our medium-sized wooden cabin 003. Perfect for families of up to 4 people, this cabin offers a comfortable and inviting space with all modern amenities. Inside, you will find warm and inviting interiors crafted from high-quality wood, a comfortable living area, a fireplace, and a fully-equipped kitchen. The bedrooms feature plush beds and spa-like bathrooms. The cabin has a private deck with a hot tub and outdoor seating area, perfect for taking in the natural surroundings.",
+      "Experience luxury family living in our medium-sized bungalow. Perfect for families of up to 4 people, this bungalow offers a comfortable and inviting space with all modern amenities. Each bungalow can accommodate up to 2 king size beds and an additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "004",
     maxCapacity: 4,
     regularPrice: 500,
     discount: 50,
-    image: imageUrl + "cabin-004.jpg",
+    image: imageUrl + "cabin-standard.webp",
     description:
-      "Indulge in the ultimate luxury family vacation in this medium-sized cabin 004. Designed for families of up to 4, this cabin offers a sumptuous retreat for the discerning traveler. Inside, the cabin boasts of opulent interiors crafted from the finest quality wood, a comfortable living area, a fireplace, and a fully-equipped gourmet kitchen. The bedrooms are adorned with plush beds and spa-inspired en-suite bathrooms. Step outside to your private deck and soak in the natural surroundings while relaxing in your own hot tub.",
+      "Experience luxury family living in our medium-sized bungalow. Perfect for families of up to 4 people, this bungalow offers a comfortable and inviting space with all modern amenities. Each bungalow can accommodate up to 2 king size beds and an additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "005",
     maxCapacity: 6,
     regularPrice: 350,
     discount: 0,
-    image: imageUrl + "cabin-005.jpg",
+    image: imageUrl + "cabin-standard.webp",
     description:
-      "Enjoy a comfortable and cozy getaway with your group or family in our spacious cabin 005. Designed to accommodate up to 6 people, this cabin offers a secluded retreat in the heart of nature. Inside, the cabin features warm and inviting interiors crafted from quality wood, a living area with fireplace, and a fully-equipped kitchen. The bedrooms are comfortable and equipped with en-suite bathrooms. Step outside to your private deck and take in the natural surroundings while relaxing in your own hot tub.",
+      "Experience luxury family living in our medium-sized bungalow. Perfect for families or groups of up to 6 people, this bungalow offers a comfortable and inviting space with all modern amenities. Each bungalow can accommodate up to 3 double size beds and an additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "006",
     maxCapacity: 6,
     regularPrice: 800,
     discount: 100,
-    image: imageUrl + "cabin-006.jpg",
+    image: imageUrl + "cabin-standard.webp",
     description:
-      "Experience the epitome of luxury with your group or family in our spacious wooden cabin 006. Designed to comfortably accommodate up to 6 people, this cabin offers a lavish retreat in the heart of nature. Inside, the cabin features opulent interiors crafted from premium wood, a grand living area with fireplace, and a fully-equipped gourmet kitchen. The bedrooms are adorned with plush beds and spa-like en-suite bathrooms. Step outside to your private deck and soak in the natural surroundings while relaxing in your own hot tub.",
+      "Experience luxury family living in our medium-sized bungalow. Perfect for families or groups of up to 6 people, this bungalow offers a comfortable and inviting space with all modern amenities. Each bungalow can accommodate up to 3 double size beds and an additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "007",
     maxCapacity: 8,
-    regularPrice: 600,
+    regularPrice: 1000,
     discount: 100,
-    image: imageUrl + "cabin-007.jpg",
+    image: imageUrl + "cabin-deluxe.webp",
     description:
-      "Accommodate your large group or multiple families in the spacious and grand wooden cabin 007. Designed to comfortably fit up to 8 people, this cabin offers a secluded retreat in the heart of beautiful forests and mountains. Inside, the cabin features warm and inviting interiors crafted from quality wood, multiple living areas with fireplace, and a fully-equipped kitchen. The bedrooms are comfortable and equipped with en-suite bathrooms. The cabin has a private deck with a hot tub and outdoor seating area, perfect for taking in the natural surroundings.",
+      "Experience luxury family living in our large-sized bungalow. Perfect for families or groups of up to 8 people, this bungalow offers a comfortable and inviting space with all modern amenities. Each bungalow can accommodate up to 3 queen size beds and an additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
   {
     name: "008",
     maxCapacity: 10,
     regularPrice: 1400,
     discount: 0,
-    image: imageUrl + "cabin-008.jpg",
+    image: imageUrl + "cabin-deluxe.webp",
     description:
-      "Experience the epitome of luxury and grandeur with your large group or multiple families in our grand cabin 008. This cabin offers a lavish retreat that caters to all your needs and desires. The cabin features an opulent design and boasts of high-end finishes, intricate details and the finest quality wood throughout. Inside, the cabin features multiple grand living areas with fireplaces, a formal dining area, and a gourmet kitchen that is a chef's dream. The bedrooms are designed for ultimate comfort and luxury, with plush beds and en-suite spa-inspired bathrooms. Step outside and immerse yourself in the beauty of nature from your private deck, featuring a luxurious hot tub and ample seating areas for ultimate relaxation and enjoyment.",
+      "Indulge in the epitome of opulence and grandeur with your large group or multiple families in our Premier Cabin. This cabin offers a lavish retreat that caters to all your needs and desires. The cabin features an opulent design and boasts of high-end finishes, intricate details and the finest quality materials throughout. The Premier Cabin can accommodate up to 4 queen size beds and an additional single bed, and is equipped with: air conditioning, ceiling fan, telephone, free Wi-Fi, satellite LCD TV, safe, minibar, tea and coffee service, iron and ironing board, walk-in closet, bathroom with rain shower, hairdryer and separate toilets.",
   },
 ];
